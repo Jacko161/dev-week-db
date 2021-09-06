bfe6c27f4dbbc3214cd56c5ef447bc57a6bc2435
Jackson Fleming <jacksonrobertfleming161@gmail.com>
2021-09-06 21:10:24 +1000

    Remove syntax error

--- sql/V3__Add_greeting_tracking.sql
+++ sql/V3__Add_greeting_tracking.sql
@@ -1,5 +1,5 @@
 CREATE TABLE dev_week.greets (
     id uuid DEFAULT uuid_generate_v4 (),
-    username; VARCHAR NOT NULL,
+    username VARCHAR NOT NULL,
     greeting_time TIMESTAMPTZ NOT NULL default now()
 );
